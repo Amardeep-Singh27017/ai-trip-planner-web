fc71c622de8313fafb12b2add82ab82a95df4870
Amardeep-Singh27017 <as5542131@gmail.com>
2025-06-03 17:20:28 +0530

    updated the user

--- src/my-trips/AllTrips.jsx
+++ src/my-trips/AllTrips.jsx
@@ -9,7 +9,6 @@ const AllTrips = () => {
     const navigation = useNavigation();
 
     const [userTrips, setUserTrips] = useState([])
-    const [loading, setLoading] = useState(true); // loading state
 
     useEffect(() => {
         GetUserTrips();
@@ -21,48 +20,30 @@ const AllTrips = () => {
             navigation('/');
             return;
         }
-
+       
         const q = query(collection(db, 'AITrips'), where('userEmail', '==', user?.email));
 
         const querySnapshot = await getDocs(q)
-        const trips = [];
+         setUserTrips([])
         querySnapshot.forEach((doc) => {
-            trips.push(doc.data());
-        });
-        setUserTrips(trips); // Set all at once
-        setLoading(false);   // After setting trips
+            setUserTrips(prevVal => [...prevVal, doc.data()])
+        })
     }
-
-
-
 
     return (
         <div className='sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10'>
             <h2 className='font-bold text-3xl'>My Trips</h2>
 
             <div className='grid grid-cols-2 md:grid-cols-3 mt-7'>
+                {userTrips?.length>0?userTrips.map((trip, index) => (
+                    <UserTripCardItem trip={trip} />
+                )):
+                [1,2,3,4,5,6].map((item,index)=>(
+                    <div key={index} className='h-[195px] w-[300px] bg-slate-200 mt-5 animate-pulse rounded-xl'>
 
-                {loading ? (
-                    // Show loading skeletons
-                    Array.from({ length: 6 }).map((_, index) => (
-                        <div
-                            key={index}
-                            className='h-[195px] w-[300px] bg-slate-200 mt-5 animate-pulse rounded-xl'
-                        ></div>
-                    ))
-                ) : userTrips && userTrips.length === 0 ? (
-                    // No trips
-                    <div className='col-span-2 md:col-span-3 text-center text-gray-600 mt-10'>
-                        You haven&apos;t created any trips yet.
                     </div>
-                ) : (
-                    // Show trips
-                    userTrips.map((trip, index) => (
-                        <UserTripCardItem key={index} trip={trip} />
-                    ))
-                )}
-
-
+                ))
+                }
             </div>
         </div>
     )
