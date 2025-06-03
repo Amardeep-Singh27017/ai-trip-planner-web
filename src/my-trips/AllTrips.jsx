58d6aea80759a0d68d538389cf2f990fcf392e1c
Amardeep-Singh27017 <as5542131@gmail.com>
2025-06-03 15:37:04 +0530

    update all trip component

--- src/my-trips/AllTrips.jsx
+++ src/my-trips/AllTrips.jsx
@@ -30,26 +30,12 @@ const AllTrips = () => {
         })
     }
 
-
-
-
     return (
         <div className='sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10'>
             <h2 className='font-bold text-3xl'>My Trips</h2>
 
             <div className='grid grid-cols-2 md:grid-cols-3 mt-7'>
-                {/* {userTrips?.length>0?userTrips.map((trip, index) => (
-                    <UserTripCardItem trip={trip} />
-                )):
-                [1,2,3,4,5,6].map((item,index)=>(
-                    <div key={index} className='h-[195px] w-[300px] bg-slate-200 mt-5 animate-pulse rounded-xl'>
-
-                    </div>
-                ))
-                } */}
-
-
-
+    
                 {userTrips === undefined ? (
                     // Loading state
                     [1, 2, 3, 4, 5, 6].map((item, index) => (
@@ -65,12 +51,15 @@ const AllTrips = () => {
                     </div>
                 ) : (
                     // Show trips
-                    userTrips.map((trip, index) => (
-                        <UserTripCardItem key={index} trip={trip} />
-                    ))
+                    userTrips?.length > 0 ? userTrips.map((trip, index) => (
+                        <UserTripCardItem trip={trip} />
+                    )) :
+                        [1, 2, 3, 4, 5, 6].map((item, index) => (
+                            <div key={index} className='h-[195px] w-[300px] bg-slate-200 mt-5 animate-pulse rounded-xl'>
+
+                            </div>
+                        ))
                 )}
-
-
             </div>
         </div>
     )
